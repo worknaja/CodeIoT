746b47a51d68b80b5f7114fb389bdbd2e8bff094
worknaja <nockandriod@gmail.com>
2018-11-08 17:50:40 +0700

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.nsx.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //        Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment())
+                    .commit();
+        }
+    }
+}
